7dab3faa2e6f727a8965089bc460f48c081e0b5f
Fran Zekan <zekan.fran369@gmail.com>
2018-12-29 13:54:45 +0100

    Add custom flashing util for macos and linux

---
+++ upload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+esptool.py --chip esp32 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 ./bootloader.GENERIC.bin 0x90000 ./lua_rtos.bin 0x8000 ./partitions.bin
